f2a12aa660753de513630c5c54fd5f8da2d03822
txya900619 <wayne900619@gmail.com>
2019-12-24 01:02:05 +0800

    add migrations.go to migration database in server begin

---
+++ database/migrations.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"log"
+)
+
+func Migration() {
+	m, err := migrate.New(
+		"file://home/MaJaJalist/migrations",
+		"postgres://postgres:postgres@database:5432/MaJaJalist?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-pg/pg/v9 v9.1.0
+	github.com/golang-migrate/migrate/v4 v4.7.1
 )
