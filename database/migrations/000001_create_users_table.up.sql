2d5ae326beea823fa3dca6a6d1757dc1bc872e97
txya900619 <wayne900619@gmail.com>
2019-12-26 11:56:28 +0800

    add base mutation and query and user and todo schema

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
-   uid serial PRIMARY KEY,
-   username VARCHAR (50) UNIQUE NOT NULL,
+   id VARCHAR (30) PRIMARY KEY,
+   username VARCHAR (30) UNIQUE NOT NULL,
    password VARCHAR (50) NOT NULL,
    email VARCHAR (300) UNIQUE NOT NULL
 );
