34d5c4c83969b167537decabacf83859cae7be24
txya900619 <wayne900619@gmail.com>
2019-12-29 17:34:13 +0800

    complete gqlgen schema (Temp

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users(
    passWord VARCHAR (50) NOT NULL,
    email VARCHAR (100) UNIQUE NOT NULL,
    avatar VARCHAR (256) NOT NULL DEFAULT 'https://i.stack.imgur.com/34AD2.jpg',
-   admin boolean NOT NULL DEFAULT FALSE ,
+   userLevel INT NOT NULL DEFAULT 0 ,
    special boolean NOT NULL DEFAULT FALSE ,
    token VARCHAR (30) NOT NULL UNIQUE ,
    emailVerified boolean NOT NULL DEFAULT FALSE ,
--- database/migrations/000002_create_todos_table.up.sql
+++ database/migrations/000002_create_todos_table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS todos
     id          VARCHAR(30) PRIMARY KEY,
     textContent VARCHAR(100),
     authorID    VARCHAR(30) NOT NULL,
-    finished    boolean     NOT NULL DEFAULT false,
-    complete    boolean     NOT NULL DEFAULT false,
+    completed   boolean     NOT NULL DEFAULT false,
     deleted     boolean     NOT NULL DEFAULT false
 );
