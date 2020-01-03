2fe1a7457143d12df5dc358574ef4ecdd1e3c2e2
txya900619 <wayne900619@gmail.com>
2020-01-03 16:05:51 +0800

    hot fix cert has problem try to fix

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/NPC-GO/MaJaJalist-backend/database"
 	"github.com/NPC-GO/MaJaJalist-backend/router"
 	"github.com/go-pg/pg/v9"
@@ -18,6 +19,7 @@ func main() {
 	server := router.InitRouter()
 	err := http.ListenAndServeTLS(":443", "./certs/server.crt", "./certs/server.key", server)
 	if err != nil {
+		fmt.Println(err)
 		http.ListenAndServe(":80", server)
 	}
 }
